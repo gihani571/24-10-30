fc2d5f5c039bc6c505fc318201e5db9236c82a83
gihani571 <gihanimadhubashini75@gmail.com>
2024-10-30 10:06:01 +0530

    2nd commit

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log(Math.floor((Math.random()*10)+1));
+const guessNumber = Math.floor((Math.random()*10)+1);
+console.log(guessNumber)
+let countNumber = 3;
+count.innerHTML = "3"
+function guess() {
+   
+
+    let txtGuessValue = Number(document.getElementById("txtGuess").value);
+  
+
+    if(txtGuessValue == guessNumber){
+        answer.innerHTML = "WIN";
+      //  console.log("correct")
+    }else{
+        answer.innerHTML = "LOSS";
+
+    }
+   
+
+}
