603a94f439d50d9fec655ee1111b31e9e03cee32
gihani571 <gihanimadhubashini75@gmail.com>
2024-11-18 09:19:37 +0530

    add new js part

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
-const guessNumber = Math.floor((Math.random()*10)+1);
-    console.log(guessNumber);
-    let countNum = 3;
-    
+const guessNumber = Math.floor(Math.random() * 10) + 1;
+console.log(guessNumber);
+let countNum = 3;
 
-    
-    function guess(){
-    if(countNum>0){
-        let guessText = Number(document.getElementById("txtGuess").value);  
-   
-        while(guessText!==guessNumber){
-            // console.log("yes");
-           
-            countNum = countNum - 1;
-            count.innerHTML = countNum;
-            console.log()
-            break;
-        }
-        if(guessText==guessNumber){
+function guess() {
+    let guessText = Number(document.getElementById("txtGuess").value);  
+
+
+    if (countNum > 0) {
+        if (guessNumber === guessText) {
+            countNum = 0;
+            chance.innerHTML = "";
             answer.innerHTML = "You WIN😜";
-            
-        }else{
-            answer.innerHTML = "You Loss🙁";
-
+        } else {
+            countNum--;
+            count.innerHTML = countNum;
+            if (guessNumber > guessText) {
+                answer.innerHTML = "Your number is too low....TRY Again!";
+            } else {
+                answer.innerHTML = "Your number is too high....TRY Again!";
+            }
         }
-        if(countNum == 0){
-            answer.innerHTML = "Game Over!🥳 <br> The  Number is " + guessNumber;
-             
+        
+        if (countNum === 0 && guessNumber !== guessText) {
+            countNum = 0;
+            chance.innerHTML = "";
+            answer.innerHTML = "You Loss🙁 Correct number is : "+guessNumber;
         }
-       
     }
-
 }
